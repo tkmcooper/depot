b13b4dc058086f32ff0deb380e9d62188768ed65
Terry Cooper <coopertkm@gmail.com>
2026-02-08 20:21:32 +0000

    Data validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,17 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validate :acceptable_image
   has_one_attached :image
   after_commit -> { broadcast_refresh_later_to "products" }
+
+  def acceptable_image
+    return unless image.attached?
+
+    acceptable_types = [ "image/gif", "image/jpeg", "image/png" ]
+    unless acceptable_types.include?(image.content_type)
+      errors.add(:image, "must be a GIF, JPG or PNG image")
+    end
+  end
 end
