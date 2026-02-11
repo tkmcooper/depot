382c47852a0b7f64e4468e0880f989f02b38809d
Terry Cooper <coopertkm@gmail.com>
2026-02-11 17:28:24 +0000

    Local change to use GBP

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module Depot
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
-
+    # Set default locale to UK English (£)
+    config.i18n.default_locale = :'en-GB'
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
