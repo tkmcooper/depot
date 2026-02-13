987d2072a69ea7769e76a16b04fd748bb8434965
Terry Cooper <coopertkm@gmail.com>
2026-02-13 22:37:35 +0000

    Line Items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,12 @@
 class Product < ApplicationRecord
+  has_many :line_items
   validates :title, :description, :image, presence: true
   validates :title, uniqueness: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validate :acceptable_image
   has_one_attached :image
   after_commit -> { broadcast_refresh_later_to "products" }
+  before_destroy :ensure_not_referenced_by_any_line_items
 
   def acceptable_image
     return unless image.attached?
@@ -14,4 +16,13 @@ class Product < ApplicationRecord
       errors.add(:image, "must be a GIF, JPG or PNG image")
     end
   end
+
+  private
+    # Ensure there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_items
+      unless line_items.empty?
+        errors.add(:base, "Line items present")
+        throw :abort
+      end
+    end
 end
